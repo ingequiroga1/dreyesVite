e36d93893cf0fcbc01848009793f1774ed8d772c
ingequiroga1 <ingequiroga1@gmail.com>
2024-04-03 08:02:36 -0600

    Se agrega aviso de privacidad

---
+++ assets/AboutView-BuY7d1o5.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t,a as c}from"./index-u9M9gtht.js";const s={},n=c("h1",null,"About",-1),_=[n];function a(r,l){return t(),o("div",null,_)}const i=e(s,[["render",a]]);export{i as default};
